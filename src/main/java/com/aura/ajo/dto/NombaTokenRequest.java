72b06a48af33edb35b3225be3be299c0d4ea624a
Gloria Obiorah <glo.obiorah@gmail.com>
2026-06-30 15:18:06 +0100

    feat: Implement NombaServiceImpl for Nomba API interactions and token management

---
+++ src/main/java/com/aura/ajo/dto/NombaTokenRequest.java
@@ -0,0 +1,23 @@
+package com.aura.ajo.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NombaTokenRequest {
+
+    @JsonProperty("grant_type")
+    private String grantType;
+
+    @JsonProperty("client_id")
+    private String clientId;
+
+    @JsonProperty("client_secret")
+    private String clientSecret;
+}
