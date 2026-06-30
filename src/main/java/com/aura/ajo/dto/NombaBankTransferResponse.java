282e2a2b53b87f26e346ad29ebd7bb3e213485a6
Gloria Obiorah <glo.obiorah@gmail.com>
2026-06-30 15:16:54 +0100

    feat: Add NombaBankTransferResponse DTO for handling bank transfer responses

---
+++ src/main/java/com/aura/ajo/dto/NombaBankTransferResponse.java
@@ -0,0 +1,38 @@
+package com.aura.ajo.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class NombaBankTransferResponse {
+
+    private String code;
+    private String description;
+    private BankTransferData data;
+
+    @Data
+    @NoArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class BankTransferData {
+        private String id;
+        private BigDecimal amount;
+        private BigDecimal fee;
+        private String type;
+        private String status;
+        private String timeCreated;
+        private BankTransferMeta meta;
+
+        @Data
+        @NoArgsConstructor
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        public static class BankTransferMeta {
+            private String merchantTxRef;
+            private String rrn;
+        }
+    }
+}
