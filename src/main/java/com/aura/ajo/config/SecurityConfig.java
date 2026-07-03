80e6af73cab8c40e5bd45066cdecf8d1aa624b59
Gloria Obiorah <glo.obiorah@gmail.com>
2026-07-03 10:59:10 +0100

    feat: Import ApiKeyAuthFilter in SecurityConfig for authentication setup

--- src/main/java/com/aura/ajo/config/SecurityConfig.java
+++ src/main/java/com/aura/ajo/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.aura.ajo.config;
 
+import com.aura.ajo.filter.ApiKeyAuthFilter;
 import com.aura.ajo.repository.IntegratorRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
