8eefee690634c3c9df92d3fca2352d22cc8e54e0
Gloria Obiorah <glo.obiorah@gmail.com>
2026-07-03 12:39:15 +0100

    feat: Add methods to find SavingsGroup by integrator ID and list by integrator ID

--- src/main/java/com/aura/ajo/repository/SavingsGroupRepository.java
+++ src/main/java/com/aura/ajo/repository/SavingsGroupRepository.java
@@ -4,10 +4,15 @@ import com.aura.ajo.enums.GroupStatus;
 import com.aura.ajo.entity.SavingsGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface SavingsGroupRepository extends JpaRepository<SavingsGroup, UUID> {
 
     Optional<SavingsGroup> findByIdAndStatus(UUID id, GroupStatus status);
+
+    Optional<SavingsGroup> findByIdAndIntegratorId(UUID id, UUID integratorId);
+
+    List<SavingsGroup> findByIntegratorIdOrderByCreatedAtDesc(UUID integratorId);
 }
