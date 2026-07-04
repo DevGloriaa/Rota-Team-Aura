5e7295d1c9a6967b6f0d01307432f23f3c456ede
Gloria Obiorah <glo.obiorah@gmail.com>
2026-07-04 11:18:46 +0100

    feat: Add security scheme configuration for API key in OpenAPI documentation

--- src/main/java/com/aura/ajo/config/OpenApiConfig.java
+++ src/main/java/com/aura/ajo/config/OpenApiConfig.java
@@ -1,8 +1,11 @@
 package com.aura.ajo.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,6 +21,12 @@ public class OpenApiConfig {
                         .version("1.0.0")
                         .contact(new Contact()
                                 .name("Team Aura")
-                                .email("glo.obiorah@gmail.com")));
+                                .email("glo.obiorah@gmail.com")))
+                .addSecurityItem(new SecurityRequirement().addList("ApiKey"))
+                .components(new Components().addSecuritySchemes("ApiKey",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name("X-Api-Key")));
     }
 }
