34a72d82847ed97c4a531d4560315104884862ed
Gloria Obiorah <glo.obiorah@gmail.com>
2026-07-04 11:15:44 +0100

    feat: Add Swagger annotations to IntegratorController for API documentation

--- src/main/java/com/aura/ajo/controller/IntegratorController.java
+++ src/main/java/com/aura/ajo/controller/IntegratorController.java
@@ -4,6 +4,8 @@ import com.aura.ajo.dto.ApiResponse;
 import com.aura.ajo.dto.CreateIntegratorRequest;
 import com.aura.ajo.dto.IntegratorResponse;
 import com.aura.ajo.service.IntegratorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,10 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/integrators")
 @RequiredArgsConstructor
+@Tag(name = "Integrators")
 public class IntegratorController {
 
     private final IntegratorService integratorService;
 
+    @Operation(summary = "Register as an integrator and get an API key")
     @PostMapping("/register")
     public ResponseEntity<ApiResponse<IntegratorResponse>> register(
             @Valid @RequestBody CreateIntegratorRequest request) {
