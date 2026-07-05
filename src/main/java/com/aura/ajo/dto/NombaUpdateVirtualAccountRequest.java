fc76b64f7e93ffb99202b9cfd81978113be8bbee
Gloria Obiorah <glo.obiorah@gmail.com>
2026-07-05 20:49:15 +0100

    feat: Add NombaUpdateVirtualAccountRequest DTO for virtual account updates

---
+++ src/main/java/com/aura/ajo/dto/NombaUpdateVirtualAccountRequest.java
@@ -0,0 +1,15 @@
+package com.aura.ajo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NombaUpdateVirtualAccountRequest {
+
+    private String accountName;
+}
