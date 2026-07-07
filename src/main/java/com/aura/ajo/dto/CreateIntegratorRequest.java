36c9483f781d5d50584518a9f9d206453e208516
Gloria Obiorah <glo.obiorah@gmail.com>
2026-07-07 12:25:32 +0100

    feat: Add Swagger annotation to CreateIntegratorRequest for improved API documentation

--- src/main/java/com/aura/ajo/dto/CreateIntegratorRequest.java
+++ src/main/java/com/aura/ajo/dto/CreateIntegratorRequest.java
@@ -1,5 +1,6 @@
 package com.aura.ajo.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CreateIntegratorRequest {
 
+    @Schema(description = "Name of your app or platform. Shown in the API key registration confirmation.", example = "Grace Chapel App")
     @NotBlank
     @Size(min = 2, max = 100)
     private String name;
